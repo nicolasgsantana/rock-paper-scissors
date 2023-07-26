c9fee9f66cae6529c55ce169d4fa29fb7cbd149f
nicolasgsantana <nicolas07san@gmail.com>
2023-07-26 15:12:33 -0300

    Update game logic functions

--- js/script.js
+++ js/script.js
@@ -1,68 +1,61 @@
-function toTitleCase(string){
-    const firstLetter = string.slice(0, 1);
-    return string.replace(firstLetter, firstLetter.toUpperCase());
-}
-
 function getComputerChoice(){
-    const CHOICES = ["rock", "paper", "scissors"];
+    const CHOICES = ["Rock", "Paper", "Scissors"];
     return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
 function playRound(playerChoice, computerChoice){
-    if(playerChoice === "rock") {
-        if(computerChoice === "rock"){
+    if(playerChoice === "Rock") {
+        if(computerChoice === "Rock"){
             return 0;
         }
-        else if(computerChoice === "paper"){
+        else if(computerChoice === "Paper"){
             return -1;
         }
-        else if(computerChoice === "scissors"){
+        else if(computerChoice === "Scissors"){
             return 1;
         }
     }
-    else if(playerChoice === "paper") {
-        if(computerChoice === "rock"){
+    else if(playerChoice === "Paper") {
+        if(computerChoice === "Rock"){
             return 1;
         }
-        else if(computerChoice === "paper"){
+        else if(computerChoice === "Paper"){
             return 0;
         }
-        else if(computerChoice === "scissors"){
+        else if(computerChoice === "Scissors"){
             return -1;
         }
     }
-    else if(playerChoice === "scissors") {
-        if(computerChoice === "rock"){
+    else if(playerChoice === "Scissors") {
+        if(computerChoice === "Rock"){
             return -1;
         }
-        else if(computerChoice === "paper"){
+        else if(computerChoice === "Paper"){
             return 1;
         }
-        else if(computerChoice === "scissors"){
+        else if(computerChoice === "Scissors"){
             return 0;
         }
     }
 }
 
-function game(){
-    let playerChoice;
-    let computerChoice;
+function game(playerChoice){
+    const ROUNDS = 5;
     let playerScore = 0;
     let computerScore = 0;
 
-    for(let i = 0; i < 5; i++){
-        playerChoice = prompt("Input your choice").toLowerCase();
-        computerChoice = getComputerChoice();
+    for(let i = 0; i < ROUNDS; i++){
+        let computerChoice = getComputerChoice();
 
         let result = playRound(playerChoice, computerChoice);
 
         if(result > 0) {
             playerScore++;
-            console.log(`You Win! ${toTitleCase(playerChoice)} beats ${toTitleCase(computerChoice)}`);
+            console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
         }
         else if(result < 0){
             computerScore++;
-            console.log(`You Lose! ${toTitleCase(computerChoice)} beats ${toTitleCase(playerChoice)}`);
+            console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
         }
         else {
             console.log("It's a tie!");
@@ -80,5 +73,3 @@ function game(){
     }
     console.log(`Final Scores\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
 }
-
-game();
